cd1e3155d7823586c14ee965f77df7c0bdd29516
DanielMeireles <daniel.meireles@outlook.com>
2020-09-20 11:10:14 -0300

    Adjust on Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,8 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/Feather';
 
 import { useTheme } from 'styled-components';
 import { useAuth } from '../../hooks/auth';
-import api from '../../services/api';
 
 import {
   Container,
@@ -22,28 +20,13 @@ export interface Provider {
 }
 
 const Dashboard: React.FC = () => {
-  const [providers, setProviders] = useState<Provider[]>([]);
-
   const { user } = useAuth();
   const { navigate } = useNavigation();
   const theme = useTheme();
 
-  useEffect(() => {
-    api.get('/providers').then((response) => {
-      setProviders(response.data);
-    });
-  }, []);
-
   const navigateToProfile = useCallback(() => {
     navigate('Profile');
   }, [navigate]);
-
-  const navigateToCreateAppointment = useCallback(
-    (providerId: string) => {
-      navigate('CreateAppointment', { providerId });
-    },
-    [navigate],
-  );
 
   return (
     <Container>
