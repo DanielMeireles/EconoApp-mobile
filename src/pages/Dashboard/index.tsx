cfb25c3d71147b9e6dcd30d40d04b7df57dff454
DanielMeireles <daniel.meireles@outlook.com>
2020-09-20 12:35:39 -0300

    Add card of Shopping List

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,7 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+import Moment from 'moment';
 
 import { useTheme } from 'styled-components';
 import { useAuth } from '../../hooks/auth';
@@ -11,18 +13,38 @@ import {
   UserName,
   ProfileButton,
   UserAvatar,
+  ShoppingLists,
+  ShoppingListsTitle,
+  ShoppingListContainer,
+  ShoppingListImage,
+  ShoppingListInfo,
+  ShoppingListName,
+  ShoppingListMeta,
+  ShoppingListMetaText,
 } from './styles';
+import api from '../../services/api';
 
-export interface Provider {
+export interface ShoppingList {
   id: string;
   name: string;
-  avatar_url: string;
+  description: string;
+  date: Date;
+  user_id: string;
+  image_url: string;
 }
 
 const Dashboard: React.FC = () => {
+  const [shoppingLists, setShoppingLists] = useState<ShoppingList[]>([]);
+
   const { user } = useAuth();
   const { navigate } = useNavigation();
   const theme = useTheme();
+
+  useEffect(() => {
+    api.get('/shoppinglists').then((response) => {
+      setShoppingLists(response.data);
+    });
+  }, []);
 
   const navigateToProfile = useCallback(() => {
     navigate('Profile');
@@ -43,6 +65,38 @@ const Dashboard: React.FC = () => {
           />
         </ProfileButton>
       </Header>
+
+      <ShoppingLists
+        data={shoppingLists}
+        keyExtractor={(shoppingList) => shoppingList.id}
+        ListHeaderComponent={
+          <ShoppingListsTitle>Listas de Compras</ShoppingListsTitle>
+        }
+        renderItem={({ item: shoppingList }) => (
+          <ShoppingListContainer onPress={() => {}}>
+            <ShoppingListImage
+              source={{
+                uri: shoppingList.image_url,
+              }}
+            />
+
+            <ShoppingListInfo>
+              <ShoppingListName>{shoppingList.name}</ShoppingListName>
+
+              <ShoppingListMeta>
+                <Icon
+                  name="calendar"
+                  size={14}
+                  color={theme.colors.cardElement}
+                />
+                <ShoppingListMetaText>
+                  {Moment(shoppingList.date).format('DD/MM/YYYY')}
+                </ShoppingListMetaText>
+              </ShoppingListMeta>
+            </ShoppingListInfo>
+          </ShoppingListContainer>
+        )}
+      />
     </Container>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RectButton } from 'react-native-gesture-handler';
+
+import { ShoppingList } from './index';
 
 export const Container = styled.View`
   flex: 1;
@@ -33,4 +37,55 @@ export const UserAvatar = styled.Image`
   width: 56px;
   height: 56px;
   border-radius: 28px;
+`;
+
+export const ShoppingLists = styled(
+  FlatList as new () => FlatList<ShoppingList>,
+)`
+  padding: 32px 24px 16px;
+`;
+
+export const ShoppingListsTitle = styled.Text`
+  font-size: 24px;
+  margin-bottom: 24px;
+  color: ${({ theme }) => theme.colors.primaryText};
+  font-family: ${({ theme }) => theme.fonts.medium};
+`;
+
+export const ShoppingListContainer = styled(RectButton)`
+  background: ${({ theme }) => theme.colors.cardBackground};
+  border-radius: 10px;
+  padding: 20px;
+  margin-bottom: 16px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ShoppingListImage = styled.Image`
+  width: 72px;
+  height: 72px;
+  border-radius: 36px;
+`;
+
+export const ShoppingListInfo = styled.View`
+  flex: 1;
+  margin-left: 24px;
+`;
+
+export const ShoppingListName = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.medium};
+  color: ${({ theme }) => theme.colors.cardTitle};
+  font-size: 18px;
+`;
+
+export const ShoppingListMeta = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 8px;
+`;
+
+export const ShoppingListMetaText = styled.Text`
+  margin-left: 8px;
+  color: ${({ theme }) => theme.colors.cardText};
+  font-family: ${({ theme }) => theme.fonts.regular};
 `;
